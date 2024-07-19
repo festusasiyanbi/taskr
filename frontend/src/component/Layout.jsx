609689e44cd2bb453ea0b1f8420f18497df79971
WUDKVN <wudkevin@icloud.com>
2024-07-18 21:16:01 -0400

    create a layout folder in which we have the header and footer of the pages

---
+++ frontend/src/component/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import '../styles/global.css'
+const Layout = ({children}) => {
+  return (
+    <div className='layout'>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+    </div>
+  )
+}
+
+export default Layout
