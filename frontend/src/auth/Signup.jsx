6be644379205b4dcf502793a7b8d3a0ae3d27a2a
Ayowolemi Eni-omosule <ayowolemieniomosule@gmail.com>
2024-07-19 11:36:31 -0400

    Modified the signup page

---
+++ frontend/src/auth/Signup.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "../styles/signup.css"
+import { Link } from 'react-router-dom'
+
+
+
+const Signup = () => {
+  return (
+    <div className='user'>
+      <h3>Taskr</h3>
+      <form className='userForm'>
+        <div className='inputs'>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' id='name'
+            autocomplete='off'
+            placeholder='Enter your name'
+            />
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email'
+            autocomplete='off'
+            placeholder='Enter your email'
+            />
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password'
+            autocomplete='off'
+            placeholder='Enter your password'
+            />
+            <button type="submit"  color='purple'>SignUp</button>
+        </div>
+      </form>
+      <div className='login'>
+        <p>Already have an account? </p>
+        <Link to ="/login" type="submit" class="btn btn-success">Login</Link>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Signup
